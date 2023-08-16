9f4a0a144b237377a689bbec3798ae8b277e2674
phb29 <paulohenriquephb29@gmail.com>
2023-08-16 16:47:09 -0300

    extração  sql txt

---
+++ BuscaConsulta/EM.BuscaConsultaAdhoc/Program.cs
@@ -0,0 +1,57 @@
+﻿using FirebirdSql.Data.FirebirdClient;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //Passar via args o diretorioEntrada, o diretorioSaida. E também o data source, a Password e o port number da string de conexão
+        //Exemplo: EM.BuscaConsultaAdhoc <diretorioEntrada> <diretorioSaida> <data source> <Password> <port number>
+
+        string consulta = "SELECT CADHID, CADHNOME, CADHDEFINICAO FROM TBCONSULTAADHOC WHERE CADHDEFINICAO LIKE '%TBDUPLICATAMATRICULA%' OR CADHDEFINICAO LIKE '%TBDUPLICATAAVULSO%' OR CADHDEFINICAO LIKE '%TBDUPLICATAAVULSO%' OR CADHDEFINICAO LIKE '%TBDUPLICATACURSOSUPERIOR%'";
+
+        string diretorioEntrada = @".\EM.BuscaConsultaAdhoc.exe C:\DADOSTESTE C:\Work\ArquivoSalvo localhost masterkey 3054:\DADOSTESTE";
+        string diretorioSaida = @"C:\Work\ArquivoSalvo";
+
+        ProcessarDiretorio(diretorioEntrada, diretorioSaida, consulta);
+    }
+
+    static void ProcessarDiretorio(string diretorioEntrada, string diretorioSaida, string consulta)
+    {
+        string[] arquivos = Directory.GetFiles(diretorioEntrada, "*.FB4");
+        string[] subDiretorios = Directory.GetDirectories(diretorioEntrada);
+
+        foreach (string caminhoArquivo in arquivos)
+        {
+            ProcessarArquivo(caminhoArquivo, diretorioSaida, consulta);
+        }
+
+        foreach (string subDiretorio in subDiretorios)
+        {
+            ProcessarDiretorio(subDiretorio, diretorioSaida, consulta);
+        }
+    }
+
+    static void ProcessarArquivo(string caminhoArquivo, string diretorioSaida, string consulta)
+    {
+        string nomeArquivo = Path.GetFileNameWithoutExtension(caminhoArquivo).ToUpper();
+
+        string stringConexao = $"initial catalog={caminhoArquivo};data source=localhost;port number=3054;Charset=ISO8859_1;User=SYSDBA;Password=masterkey;";
+
+        using FbConnection conexao = new(stringConexao);
+        conexao.Open();
+
+        using FbCommand comando = new(consulta, conexao);
+        using FbDataReader leitor = comando.ExecuteReader();
+
+        while (leitor.Read())
+        {
+            string cadhID = ((string)leitor["CADHID"]);
+            string cadhDefinicao = ((string)leitor["CADHDEFINICAO"]);
+
+            cadhDefinicao = cadhDefinicao.Replace("\\r\\n", "").Replace("\\t\\t", "").Replace("\\r\\n\\t\\t\\t\\t\\t\\t\\t\\", "").Trim();
+
+            string caminhoTxt = Path.Combine(diretorioSaida, $"{nomeArquivo}.{cadhID}.txt");
+            File.WriteAllText(caminhoTxt, cadhDefinicao);
+        }
+    }
+}
---
+++ BuscaConsultaViaArgumento/EM.BuscaConsultaAdhoc/Program.cs
@@ -0,0 +1,74 @@
+﻿
+using FirebirdSql.Data.FirebirdClient;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        if (args.Length < 5)
+        {
+            Console.WriteLine("EM.BuscaConsultaAdhoc <diretorioEntrada> <diretorioSaida> <data source> <Password> <port number>");
+            return;
+        }
+
+        string diretorioEntrada = args[0];
+        string diretorioSaida = args[1];
+        string dataSource = args[2];
+        string password = args[3];
+        int portNumber = int.Parse(args[4]);
+
+        string consulta = "SELECT CADHID, CADHNOME, CADHDEFINICAO FROM TBCONSULTAADHOC WHERE CADHDEFINICAO LIKE '%TBDUPLICATAMATRICULA%' OR CADHDEFINICAO LIKE '%TBDUPLICATAAVULSO%' OR CADHDEFINICAO LIKE '%TBDUPLICATAAVULSO%' OR CADHDEFINICAO LIKE '%TBDUPLICATACURSOSUPERIOR%'";
+
+        ProcessarDiretorio(diretorioEntrada, diretorioSaida, consulta, dataSource, password, portNumber);
+    }
+
+    static void ProcessarDiretorio(string diretorioEntrada, string diretorioSaida, string consulta, string dataSource, string password, int portNumber)
+    {
+        string[] arquivos = Directory.GetFiles(diretorioEntrada, "*.FB4");
+        string[] subDiretorios = Directory.GetDirectories(diretorioEntrada, "*", SearchOption.AllDirectories);
+
+        foreach (string caminhoArquivo in arquivos)
+        {
+            ProcessarArquivo(caminhoArquivo, diretorioSaida, consulta, dataSource, password, portNumber);
+        }
+
+        foreach (string subDiretorio in subDiretorios)
+        {
+            ProcessarDiretorio(subDiretorio, diretorioSaida, consulta, dataSource, password, portNumber);
+        }
+    }
+
+    static void ProcessarArquivo(string caminhoArquivo, string diretorioSaida, string consulta, string dataSource, string password, int portNumber)
+    {
+        string nomeArquivo = Path.GetFileNameWithoutExtension(caminhoArquivo).ToUpper();
+
+        string stringConexao = $"initial catalog={caminhoArquivo};data source={dataSource};port number={portNumber};Charset=ISO8859_1;User=SYSDBA;Password={password};";
+
+        using FbConnection conexao = new(stringConexao);
+        conexao.Open();
+
+        using FbCommand comando = new(consulta, conexao);
+        using FbDataReader leitor = comando.ExecuteReader();
+        string nomeArquivoSaida = $"{nomeArquivo}.txt";
+        string caminhoTxt = Path.Combine(diretorioSaida, nomeArquivoSaida);
+
+        using StreamWriter writer = new(caminhoTxt);
+        while (leitor.Read())
+        {
+            string cadhID = ((string)leitor["CADHID"]);
+            string cadhDefinicao = ((string)leitor["CADHDEFINICAO"]);
+
+            cadhDefinicao = cadhDefinicao.Replace("\\r\\n", "").Replace("\\t\\t", "").Replace("\\r\\n\\t\\t\\t\\t\\t\\t\\t\\", "").Trim();
+
+            writer.WriteLine($"Cadh ID: {cadhID}");
+            writer.WriteLine($"Definição: {cadhDefinicao}");
+            writer.WriteLine();
+        }
+
+        if (writer.BaseStream.Length == 0)
+        {
+            writer.Close();
+            File.Delete(caminhoTxt);
+        }
+    }
+}
